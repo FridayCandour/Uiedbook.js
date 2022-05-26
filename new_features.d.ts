9361822c749d1337f4678953ff380204d5e9fed9
fridaycandour <fridaymichaels662@gmail.com>
2022-05-26 15:29:51 +0100

    latest stable version

--- new_features.d.ts
+++
@@ -1,7 +0,0 @@
-declare type props = {
-    multiple: boolean;
-};
-export declare function getContacts(config: props): Promise<string | string[] | boolean>;
-export declare function useDeviceMotion(fuc: (ev: DeviceMotionEvent) => void): void;
-export declare function useFileSystem(): void;
-export {};
